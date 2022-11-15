d6e974d6c0df1de4e252b88e31c5d2ab50779a1d
NikitaDunskiy <nikita.dunskiy@gmail.com>
2022-11-16 01:41:41 +0300

    added task3 array

--- app/src/main/java/com/example/homework3/HomeworkArrays.java
+++ app/src/main/java/com/example/homework3/HomeworkArrays.java
@@ -1,4 +1,41 @@
 package com.example.homework3;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class HomeworkArrays {
+    public static void main(String[] args) {
+        taskThreeArrays();
+    }
+
+    static void taskThreeArrays() {
+        int n = 20;
+        int[] array = new int[n];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = ((int) (Math.random() * 20));
+        }
+        System.out.println(Arrays.toString(array));
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите число, которое хотите удалить из исходного массива: ");
+        int del = in.nextInt();
+        int count = 0;
+        for (int needDel : array) {
+            if (needDel == del) {
+                count++;
+            }
+        }
+        if (count == 0) {
+            System.out.println("Такого числа нет в массиве");
+        } else {
+            int[] newArray = new int[n - count];
+            int temp = 0;
+            for (int i : array) {
+                if (i != del) {
+                    newArray[temp] = i;
+                    temp++;
+                }
+            }
+            System.out.println("Массив без заданного числа - " + Arrays.toString(newArray));
+        }
+    }
 }

401d09ef680afdc2b800ad2c7b2b72a0a7bb8e31
NikitaDunskiy <nikita.dunskiy@gmail.com>
2022-11-16 01:32:28 +0300

    added task1 array

--- app/src/main/java/com/example/homework3/HomeworkArrays.java
+++ app/src/main/java/com/example/homework3/HomeworkArrays.java
@@ -1,4 +1,15 @@
 package com.example.homework3;
 
+import java.util.Arrays;
+
 public class HomeworkArrays {
+    public static void main(String[] args) {
+        taskOneArrays();
+    }
+
+    static void taskOneArrays() {
+        String[] names = new String[]{"Nikita", "Ivan", "Igor", "Alexei"};
+        Arrays.sort(names);
+        System.out.println("Отсортированный массив имен - " + Arrays.toString(names));
+    }
 }

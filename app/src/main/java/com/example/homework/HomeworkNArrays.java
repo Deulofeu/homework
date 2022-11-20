4c4ccb1a6e5893035a84712d780599deb2efc09f
NikitaDunskiy <nikita.dunskiy@gmail.com>
2022-11-20 22:30:55 +0300

    added task3 HomeworkNArrays

--- app/src/main/java/com/example/homework/HomeworkNArrays.java
+++ app/src/main/java/com/example/homework/HomeworkNArrays.java
@@ -7,37 +7,36 @@ public class HomeworkNArrays {
     public static void main(String[] args) {
         taskOneNArrays();
         taskTwoNArrays();
+        taskThreeNArrays();
     }
 
     static void taskOneNArrays() {
         Scanner in = new Scanner(System.in);
         int size1, size2;
-        System.out.println("Задание 1");
-        System.out.print("Число строк: ");
+        System.out.println("\n" + "Задание 1" + "\n");
+        System.out.print("Число строк для двумерного массива: ");
         size1 = in.nextInt();
-        System.out.print("Число столбцов: ");
+        System.out.print("Число столбцов для двумерного массива: ");
         size2 = in.nextInt();
         int[][] twoArray = new int[size1][size2];
         int newSize = size1 * size2;
         int[] oneArray = new int[newSize];
         int temp = 0;
-        System.out.println("Двумерный массив: ");
         for (int i = 0; i < twoArray.length; i++) {
             for (int j = 0; j < twoArray[i].length; j++) {
                 twoArray[i][j] = ((int) (Math.random() * 100));
-                System.out.print(twoArray[i][j] + " ");
                 oneArray[temp] = twoArray[i][j];
                 temp++;
             }
-            System.out.println("");
         }
+        System.out.println("Двумерный массив: " + "\n" + Arrays.deepToString(twoArray));
         System.out.println("Одномерный массив из двумерного: " + Arrays.toString(oneArray));
     }
 
     static void taskTwoNArrays() {
         Scanner in = new Scanner(System.in);
         int size1, size2, size3;
-        System.out.println("Задание 2");
+        System.out.println("\n" + "Задание 2" + "\n");
         System.out.print("Введите первую размерность трехмерного массива - ");
         size1 = in.nextInt();
         System.out.print("Введите вторую размерность трехмерного массива - ");
@@ -88,5 +87,43 @@ public class HomeworkNArrays {
         }
         System.out.println("Трехмерный массив из чисел " + "\n" + Arrays.deepToString(threeArray));
         System.out.println("Трехмерный массив из слов " + "\n" + Arrays.deepToString(strThreeArray));
+    }
+
+    static void taskThreeNArrays() {
+        Scanner in = new Scanner(System.in);
+        int size1, size2;
+        System.out.println("\n" + "Задание 3" + "\n");
+        System.out.print("Число строк для двумерного массива: ");
+        size1 = in.nextInt();
+        System.out.print("Число столбцов для двумерного массива: ");
+        size2 = in.nextInt();
+        int[][] twoArray = new int[size1][size2];
+        System.out.println("Двумерный массив: ");
+        for (int i = 0; i < twoArray.length; i++) {
+            for (int j = 0; j < twoArray[i].length; j++) {
+                twoArray[i][j] = ((int) (Math.random() * 100));
+            }
+        }
+        System.out.println("Исходный двумерный массив: " + "\n" + Arrays.deepToString(twoArray));
+        int number;
+        boolean flag = true;
+        System.out.print("Введите число: ");
+        number = in.nextInt();
+        for (int i = 0; i < twoArray.length; i++) {
+            for (int j = 0; j < twoArray[i].length; j++) {
+                while (flag) {
+                    if (twoArray[i][j] % number != 0) {
+                        System.out.println(twoArray[i][j]);
+                    } else {
+                        System.out.println("Найден искомый элемент");
+                        flag = false;
+                    }
+                    break;
+                }
+            }
+        }
+        if (flag == true) {
+            System.out.println("Искомый элемент не найден");
+        }
     }
 }

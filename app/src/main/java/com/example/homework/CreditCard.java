13ad3ae3b4c9e791e2e0425a5f145113f398450e
NikitaDunskiy <nikita.dunskiy@gmail.com>
2022-11-22 23:20:03 +0300

    homework CreditCard corrected version

--- app/src/main/java/com/example/homework/CreditCard.java
+++ app/src/main/java/com/example/homework/CreditCard.java
@@ -11,25 +11,21 @@ public class CreditCard {
         this.amount = amount;
     }
 
-    public void infoAboutCard() {
+    public void printInfoAboutCard() {
         System.out.println("Номер счета: " + accountNumber + "\n" + "сумма на счете: " + amount + " BYN");
     }
 
-    int addBalance(int sum) {
+    void addBalance(int sum) {
         amount += sum;
-        return amount;
     }
 
-    int removeFromBalance(int sum) {
+    void removeFromBalance(int sum) {
         if (sum > amount) {
             System.out.println("Недостаточно денег на балансе");
-            return amount;
         } else {
             amount -= sum;
-            return amount;
         }
     }
-
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -37,9 +33,9 @@ public class CreditCard {
         CreditCard card1 = new CreditCard("BY20OLMP31350000001000000933", 0);
         CreditCard card2 = new CreditCard("BY86AKBB10100000002966000000", 10);
         CreditCard card3 = new CreditCard("BY13NBRB36009000347600002200", 20);
-        card1.infoAboutCard();
-        card2.infoAboutCard();
-        card3.infoAboutCard();
+        card1.printInfoAboutCard();
+        card2.printInfoAboutCard();
+        card3.printInfoAboutCard();
         System.out.print("Введите сумму для пополнения баланса первой карты: ");
         sumAdd = in.nextInt();
         card1.addBalance(sumAdd);
@@ -49,8 +45,8 @@ public class CreditCard {
         System.out.print("Введите сумму для списания баланса с третьей карты: ");
         sumRemove = in.nextInt();
         card3.removeFromBalance(sumRemove);
-        card1.infoAboutCard();
-        card2.infoAboutCard();
-        card3.infoAboutCard();
+        card1.printInfoAboutCard();
+        card2.printInfoAboutCard();
+        card3.printInfoAboutCard();
     }
 }
